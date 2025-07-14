1d5813d8e935fe1b99be8639a12e6c2cc1f4bd9e
Mohammad <mmfd25285@gmail.com>
2025-07-14 17:44:15 +0330

    create history file for epoches

--- show_result.py
+++ show_result.py
@@ -3,18 +3,21 @@ import pandas as pd
 from pathlib import Path
 import numpy as np
 from tqdm import tqdm
-#print(dir_history_model)
+import os
 
+
+#Create dir for save 
 csv_files = [f.name for f in dir_history_model.glob("*.csv")]
+dir_save = Path(dir_history_model,'create epoch')
+if not os.path.exists(dir_save):
+    os.makedirs(dir_save)
 
-#print(csv_files)
-
+# for all csv file in baser dir
 for file_name in tqdm(csv_files):
     
     dir_file = Path(dir_history_model, file_name)
     file = pd.read_csv(dir_file)
     if 'number/ len_data' in file.columns:
-        #print(file.index[0])
         list_data = [ i.split('/') for i in file['number/ len_data']]
         list_n = [int(j[0])/int(j[1]) for j in list_data]
         n_list = [0]
@@ -22,22 +25,28 @@ for file_name in tqdm(csv_files):
             n_list.append(number)
             if number == 1 :
                 break
-        #print(n_list)
+        
 
         df_n_list = np.array(n_list[1:])-np.array(n_list[:-1])
         file.drop('number/ len_data', axis=1 , inplace=True)
 
         for i in range(len(file)):
             file.iloc[i] = file.iloc[i] * df_n_list[(i + 1 ) % len(df_n_list)]
-        file = file[: (file.index // len(df_n_list)) * len(df_n_list)]
+        x = None
+        if len(file) % len(df_n_list) != 0: 
+            print(f'{len(file)} % {len(df_n_list)}  = {len(file) % len(df_n_list)}') 
+            print(f'epoch not complet for file {file_name}')  
+            x = input('type error to add in mane')
+            number = int((len(file) // len(df_n_list)) * len(df_n_list))
+            file = file[: number]
+        else:
+            x = None
         file_new = file.groupby(file.index // len(df_n_list)).sum()
         file_new["epoches"] = range(1, len(file_new) + 1)
         # جابجا کردن ستون‌ها: epoches اول، بقیه بعدش
         cols = ["epoches"] + [col for col in file_new.columns if col != "epoches"]
         file_new = file_new[cols]
-        file_new.to_csv(Path(dir_history_model, file_name), index=False)
+        file_new.to_csv(Path(dir_save, f'{x}_{file_name}'), index=False)
             
-        
-        #print(df_n_list) 
         
 
