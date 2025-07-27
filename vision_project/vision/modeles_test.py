8a0aff0ababbef9e4bb4649b0c50a1f5a5966b9f
Mohammad <mmfd25285@gmail.com>
2025-07-27 15:24:53 +0330

    add test image for test modeles

--- vision_project/vision/modeles_test.py
+++ vision_project/vision/modeles_test.py
@@ -169,7 +169,7 @@ def plot_random_image(
     file_path = Path(dir_history_model_google_dirve , file_path)
     file_path.parent.mkdir(parents=True, exist_ok=True)
     print('-' * 50)
-    print('dirctory to save image : {file_path}')
+    print(f'dirctory to save image : {file_path}')
 
 
     
