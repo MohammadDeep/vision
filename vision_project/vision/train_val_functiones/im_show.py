1c13b68b6f55c959165f8a9e7d911193c96acde9
Mohammad <mmfd25285@gmail.com>
2025-07-28 16:19:17 +0330

    change font size for image in plot

--- vision_project/vision/train_val_functiones/im_show.py
+++ vision_project/vision/train_val_functiones/im_show.py
@@ -63,12 +63,30 @@ def imshow_function(
 
             ax.imshow(img)
             # اندازه تصویر (بعد از تبدیل تنسور)
-            height = img.shape[0]  # معادل H در [H, W, C]
+            #height = img.shape[0]  # معادل H در [H, W, C]
 
             # محاسبه اندازه فونت بر اساس ارتفاع تصویر
             # به طور تجربی: font_size = ارتفاع تصویر / یک مقدار ثابت
-            font_size = min(12, height // 20)  # محدودیت حداقل
-            print(f'fontsize for image : {font_size}')
+            #font_size = min(12, height // 25)  # محدودیت حداقل
+            # اندازه فیزیکی ساب‌پلات
+            subplot_height_inches = fig.get_size_inches()[1] / row
+            subplot_width_inches  = fig.get_size_inches()[0] / colum
+
+            # طول تایتل به تعداد کاراکتر
+            title_length = len(title)
+
+            # نسبت به عرض ساب‌پلات، چند کاراکتر جا می‌شود؟ (حدسی: ~2.2 کاراکتر در هر اینچ)
+            max_chars_per_line = subplot_width_inches * 2.2
+
+            # اگر عنوان از ظرفیت بیشتر بود، فونت را کاهش بده
+            scale_factor = max(0.6, min(1.0, max_chars_per_line / (title_length + 1)))  # محدوده‌ی [0.6, 1.0]
+
+            # فونت پایه نسبت به اندازه فیزیکی
+            base_font_size = min(12, int(min(subplot_height_inches, subplot_width_inches) * 2.5))
+
+            # اندازه نهایی فونت
+            font_size = int(base_font_size * scale_factor)
+
             ax.set_title(title, color=color,fontsize=font_size)
             ax.axis('off')
         else:
