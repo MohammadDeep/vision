55a3883e3b39794e93d4d421e33588c4e3d4451d
Mohammad <mmfd25285@gmail.com>
2025-08-09 13:07:20 +0330

    change BATCH_SIZE = 2 * 8 num_workers = 14

--- modeles/model_8.py
+++ modeles/model_8.py
@@ -50,8 +50,8 @@ model_stucher = dic_model_8['model_stucher']
 input_shape =dic_model_8['input_shape']
 mean = dic_model_8['mean']
 std = dic_model_8['std']
-BATCH_SIZE = 300
-num_workers = 20
+BATCH_SIZE = 2 * 8
+num_workers = 14
 '''
 ==========================================================================
                                     path 2
